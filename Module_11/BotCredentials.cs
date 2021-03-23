dc734abd4aa5d317a1927bc9caac6044b117dcfc
Aleksandr Giller <alexander.p.17@yandex.ru>
2021-03-23 21:57:54 +0500

    TelegramBot

---
+++ Module_11/BotCredentials.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelegramBot
+{
+    public static class BotCredentials
+    {
+        public static readonly string BotToken = "1669123713:AAFN41UctuddQEpkIGYcI1Kai53Oo - g68kI";
+    }
+}
